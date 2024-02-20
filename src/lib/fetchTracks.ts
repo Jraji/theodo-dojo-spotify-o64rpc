ce04e1550110edbe70d70f3beb3650e4d56a3888
Jraji <jaafarr@theodo.fr>
2024-02-20 19:46:01 +0100

    Token spotify

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,4 +1,4 @@
-const apiToken: string = '';
+const apiToken: string = 'BQBK34R0BcjfNpE2_uG_V2jjpyXvNtqje_e0Nn9sT4FE2xovEkWP-mo96gc4BCIpIj-yFJTQtAuswSAhdxewBMdE6YsLrWo9s7R6TekgF1m6OMubPsRKqEfG8QeygTmIVTps6qryQPSqIafoVNyyXhg-mTRtBSjcq9lPHUlV-dUPX-WbgPucnTCsp5xQ6ErPsXD58Ljd';
 
 export const fetchTracks = async () => {
   // This function does nothing, yet
